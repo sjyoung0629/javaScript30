de9cf9d18939ac813c2ead4b1eca39b296a88ed4
Jooyoung Son <sjyoung0629@gmail.com>
2019-02-11 22:29:31 +0900

    190211 Update day03 CSS variables (add box-shadow, box-reflect style)

--- day03 - CSS variables/css_variables.js
+++ day03 - CSS variables/css_variables.js
@@ -1,10 +1,16 @@
 const inputs = document.querySelectorAll('.controls input');
+const reflect_options = document.querySelectorAll('.direct_option');
 
-function handleUpdate() {
+var handleUpdate = function () {
     // dataset: all the data attributes from that specific element
     const suffix = this.dataset.sizing || '';
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
+var handleReflect = function (e) {
+    document.documentElement.style.setProperty('box-reflect', e.target.getAttribute('name'));
+}
+
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
 inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+reflect_options.forEach(option => option.addEventListener('click', handleReflect));
